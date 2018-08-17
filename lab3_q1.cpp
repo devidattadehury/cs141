1c764efefb64b018cc7205fb0c4b55fa152bb575
devidattadehury <42206805+devidattadehury@users.noreply.github.com>
2018-08-17 06:11:31 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=1;
+char b='a';
+float c=121;
+double d=123123121231;
+bool e=1;
+cout<<a<<"size of a="<<sizeof(a)<<endl;
+cout<<b<<"size of b="<<sizeof(b)<<endl;
+cout<<c<<"size of c="<<sizeof(c)<<endl;
+cout<<d<<"size of d="<<sizeof(d)<<endl;
+cout<<e<<"size of e="<<sizeof(e)<<endl;
+return 2;
+}
---
+++ lab3_q2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=2,b=3;
+float c=1.5,d=2.5;
+cout<<"sum of a,b =" << a+b << endl;
+cout<<"sum of c,d="  << c+d << endl;
+cout<<"product of a,b=" << a*b << endl;
+cout<<"division of b,c=" << b/c << endl;
+cout<<"modulus of a,b=" << a%b << endl;
+return 1;
+}
