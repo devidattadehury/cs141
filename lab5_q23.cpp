e663f0f037e20ac59e468bdee6bb7a2e95765e84
devidattadehury <42206805+devidattadehury@users.noreply.github.com>
2018-08-31 22:38:26 +0530

    Add files via upload

---
+++ lab5_q23.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+int main () {
+     int n;
+cout << "Enter the value of n " << endl ;
+ cin >> n ; 
+
+
+   int i = n;
+  while (i>=1)
+{  
+cout << i << endl;
+  i= i-1 ;
+}
+
+
+ return 0;
+}
