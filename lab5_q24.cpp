f526e6ff3b9f183f53ccce5e5a4001d78b97f9aa
devidattadehury <42206805+devidattadehury@users.noreply.github.com>
2018-09-02 11:05:48 +0530

    Add files via upload

---
+++ lab5_q24.cpp
@@ -0,0 +1,21 @@
+/*
+*Write a C++ program to print all alphabets from a to z. - using while loop.
+*/
+
+
+#include<iostream>
+
+using namespace std;
+
+
+int main () {
+      int i =char ('a');
+while(i <= char('z'))
+{
+   cout << char (i) << endl;
+i++ ;
+}
+    return 0;
+}
+
+
