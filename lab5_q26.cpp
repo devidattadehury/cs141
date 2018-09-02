f12902eacb5ae3a0f7700c7b45ec01ca56582483
devidattadehury <42206805+devidattadehury@users.noreply.github.com>
2018-09-02 23:17:37 +0530

    Add files via upload

---
+++ lab5_q26.cpp
@@ -0,0 +1,17 @@
+/*
+*Write a C++ program to print all odd numbers between 1 to 100. - using while loop.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+ int main () {
+    int i=1 ;
+  while (i <= 100)
+{  
+  cout << i << endl ;
+  i = i+2 ;
+}
+  return 0 ;
+}
